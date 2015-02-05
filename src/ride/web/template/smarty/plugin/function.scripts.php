582db57797f60607ea8c4d4648209edb8c3573ad
Kaya <kayalion@kayalion.net>
2015-02-05 20:17:56 +0100

    use minifier service

--- src/ride/web/template/smarty/plugin/function.scripts.php
+++ src/ride/web/template/smarty/plugin/function.scripts.php
@@ -11,45 +11,14 @@ function smarty_function_scripts($params, &$smarty) {
     $tags = array();
 
     $dependencyInjector = $app['system']->getDependencyInjector();
+    $service = $dependencyInjector->get('ride\\service\\MinifierService');
 
-    foreach ($app['minifier']['scripts'] as $script => $dummy) {
-        if (substr($script, 0, 7) == 'http://' || substr($script, 0, 8) == 'https://' || substr($script, 0, 2) == '//') {
-            $tags[$script] = '<script type="text/javascript" src="' . $script . '"></script>';
-        } elseif (substr($script, 0, 7) == '<script') {
-            $tags[$script] = $script;
+    $minifiedScripts = $service->minifyJs(array_keys($app['minifier']['scripts']));
+    foreach ($minifiedScripts as $minifiedScript) {
+        if (strpos($minifiedScript, '<script') === 0) {
+            $tags[] = $minifiedScript;
         } else {
-            $minifierScripts[$script] = true;
-        }
-    }
-
-    if ($minifierScripts) {
-        $config = $dependencyInjector->get('ride\\library\\config\\Config');
-        $disabled = $config->get('minifier.disabled', true);
-
-        if ($disabled) {
-            $baseUrl = $app['url']['base'] . '/';
-
-            foreach ($minifierScripts as $script => $dummy) {
-                $tags[$script] = '<script type="text/javascript" src="' . $baseUrl . $script . '"></script>';
-            }
-        } else {
-            $fileBrowser = $app['system']->getFileBrowser();
-            $log = $dependencyInjector->get('ride\\library\\log\\Log');
-            $minifier = $dependencyInjector->get('ride\\library\\minifier\\Minifier', 'js');
-
-            $log->logDebug('Rendering minified script');
-            foreach ($minifierScripts as $script => $dummy) {
-                $log->logDebug('- ' . $script);
-            }
-
-            $minifiedScript = $minifier->minify(array_keys($minifierScripts));
-            $minifiedScript = $fileBrowser->getRelativeFile($minifiedScript, true);
-
-            $log->logDebug('Rendered minified script ' . $minifiedScript);
-
-            $minifiedScript = $app['url']['base'] . '/' . $minifiedScript;
-
-            $tags[$script] = '<script type="text/javascript" src="' . $minifiedScript . '"></script>';
+            $tags[] = '<script type="text/javascript" src="' . $minifiedScript . '"></script>';
         }
     }
 
--- src/ride/web/template/smarty/plugin/function.styles.php
+++ src/ride/web/template/smarty/plugin/function.styles.php
@@ -22,38 +22,14 @@ function smarty_function_styles($params, &$smarty) {
 
     $tags = array();
 
-    $fileBrowser = $app['system']->getFileBrowser();
     $dependencyInjector = $app['system']->getDependencyInjector();
-    $log = $dependencyInjector->get('ride\\library\\log\\Log');
-    $minifier = $dependencyInjector->get('ride\\library\\minifier\\Minifier', 'css');
+    $service = $dependencyInjector->get('ride\\service\\MinifierService');
+
     foreach ($styles as $media => $mediaStyles) {
-        $minifierStyles = array();
-
-        foreach ($mediaStyles as $style => $dummy) {
-            if (substr($style, 0, 7) == 'http://' || substr($style, 0, 8) == 'https://' || substr($style, 0, 2) == '//') {
-                $tags[$style] = '<link rel="stylesheet" type="text/css" href="' . $style . '" media="' . $media . '">';
-            } else {
-                $minifierStyles[$style] = true;
-            }
+        $minifiedStyles = $service->minifyCss(array_keys($mediaStyles));
+        foreach ($minifiedStyles as $minifiedStyle) {
+            $tags[] = '<link rel="stylesheet" type="text/css" href="' . $minifiedStyle . '" media="' . $media . '">';
         }
-
-        if (!$minifierStyles) {
-            continue;
-        }
-
-        $log->logDebug('Rendering minified style');
-        foreach ($minifierStyles as $style => $dummy) {
-            $log->logDebug('- ' . $style);
-        }
-
-        $minifiedStyle = $minifier->minify(array_keys($minifierStyles));
-        $minifiedStyle = $fileBrowser->getRelativeFile($minifiedStyle, true);
-
-        $log->logDebug('Rendered minified style ' . $minifiedStyle);
-
-        $minifiedStyle = $app['url']['base'] . '/' . $minifiedStyle;
-
-        $tags[$style] = '<link rel="stylesheet" type="text/css" href="' . $minifiedStyle . '" media="' . $media . '">';
     }
 
     return implode("\n        ", $tags);
